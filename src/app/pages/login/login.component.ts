76b8c160b9b40937ad9f8ead00531215234b5555
JorgeDeLaraH <93809477+JorgeDeLaraH@users.noreply.github.com>
2024-06-07 18:50:50 -0600

    Creado componente login y rutas

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent{
+
+  credenciales={
+    usuario:"",
+    password:""
+  }
+  
+}
